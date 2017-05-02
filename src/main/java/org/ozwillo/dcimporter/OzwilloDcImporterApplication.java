dae88f728e483da76da8473135defc6607440056
Benoit Orihuela <borihuela@ozwillo.org>
2017-05-02 10:59:18 +0200

    Bootstrap application

---
+++ src/main/java/org/ozwillo/dcimporter/OzwilloDcImporterApplication.java
@@ -0,0 +1,12 @@
+package org.ozwillo.dcimporter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OzwilloDcImporterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OzwilloDcImporterApplication.class, args);
+	}
+}
