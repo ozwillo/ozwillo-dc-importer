514649fb8fcb53b4963c1e8995916d5c78b82b40
meziane <meziane.mehdi@hotmail.com>
2017-05-05 11:43:59 +0200

    test feature2 integration ozwillo-spring

--- src/main/java/org/ozwillo/dcimporter/OzwilloDcImporterApplication.java
+++ src/main/java/org/ozwillo/dcimporter/OzwilloDcImporterApplication.java
@@ -2,8 +2,10 @@ package org.ozwillo.dcimporter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "org.oasis_eu.spring")
 public class OzwilloDcImporterApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/org/ozwillo/dcimporter/service/SynchronizerService.java
@@ -0,0 +1,40 @@
+package org.ozwillo.dcimporter.service;
+
+
+import java.util.List;
+
+import org.oasis_eu.spring.datacore.DatacoreClient;
+import org.oasis_eu.spring.datacore.model.DCResource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class SynchronizerService {
+
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(SynchronizerService.class);
+	
+    @Autowired
+    private SystemUserService systemUserService;
+
+	private DatacoreClient datacoreClient;
+
+    @Scheduled(fixedDelayString = "${application.syncDelay}")
+    public void synchronizeOrgs() {
+        
+    	systemUserService.runAs(() -> {
+
+    		List<DCResource> dcRessources = datacoreClient.findResources("oasis.main","geo:Area_0");
+    		
+    		LOGGER.info("findRessources "+dcRessources.toString());
+    		
+    	});
+                
+        
+    }
+
+}
