fac197b8cb95c4c8df796985567d6ef8abc37df1
ali boulajine <abdelaliboulajine@gmail.com>
2018-11-05 16:09:58 +0100

    [Evo] Ref #5044 - List of data access request

--- src/main/resources/public/src/views/Dashboard.vue
+++ src/main/resources/public/src/views/Dashboard.vue
@@ -1,13 +1,54 @@
 <template>
-    
+    <div id="container" class="container">
+        <h2>Dashboard</h2>
+        <table class="table table-sm">
+            <thead class="thead-dark">
+                <tr>
+                    <th>Nom</th>
+                    <th>DC Model</th>
+                    <th>Organization</th>
+                    <th>State</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="dataRequest in dataRequests">
+                    <td>{{ dataRequest.nom }}</td>
+                    <td>{{ dataRequest.model }}</td>
+                    <td>{{ dataRequest.organization }}</td>
+                    <td>{{ dataRequest.state }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </template>
 
 <script>
+    import axios from 'axios'
     export default {
-        name: "Dashboard"
+        name: "Dashboard",
+        data() {
+            return {
+              dataRequests: [],
+              errors: []
+            }
+        },
+        beforeCreate() {
+            axios.get(`api/data_access_request/123456789/sent`)
+                .then(response => {
+                    this.response = response.data
+                    this.dataRequests = this.response
+                })
+                .catch(e => {
+                    this.errors.push(e)
+                })
+        }
     }
 </script>
 
 <style scoped>
-
+.table .thead-dark th {
+  color: #fff;
+  background-color: #6f438e;
+  border-color: #32383e;
+}
 </style>
