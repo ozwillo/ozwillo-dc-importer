986efc59146bd4fa4fad51a45a7317e74445662c
Benoit Orihuela <bobeal@gmail.com>
2019-04-17 19:01:52 +0200

    fix: add missing @Repository annotation

--- src/main/kotlin/org/ozwillo/dcimporter/repository/DataAccessRequestRepository.kt
+++ src/main/kotlin/org/ozwillo/dcimporter/repository/DataAccessRequestRepository.kt
@@ -2,8 +2,10 @@ package org.ozwillo.dcimporter.repository
 
 import org.ozwillo.dcimporter.model.DataAccessRequest
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository
+import org.springframework.stereotype.Repository
 import reactor.core.publisher.Flux
 
+@Repository
 interface DataAccessRequestRepository: ReactiveMongoRepository<DataAccessRequest, String>{
 
     fun findByState(state: String): Flux<DataAccessRequest>
